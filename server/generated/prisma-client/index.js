5f40a33e644701294753063f9794aabf7c3d1e60
Erik Pehrers <erik.pehrers@jetshop.se>
2019-08-17 23:58:13 +0200

    jezz..

--- server/generated/prisma-client/index.js
+++ server/generated/prisma-client/index.js
@@ -1,22 +1,21 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var prisma_lib_1 = require('prisma-client-lib');
-var typeDefs = require('./prisma-schema').typeDefs;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
-    name: 'Product',
+    name: "Product",
     embedded: false
   },
   {
-    name: 'Order',
+    name: "Order",
     embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `${process.env['PRISMA_ENDPOINT']}`,
-  secret: `${process.env['PRISMA_SECRET']}`
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`
 });
 exports.prisma = new exports.Prisma();
