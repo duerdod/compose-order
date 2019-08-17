b37de31f42c905f284f17c743d70b163d322b850
Erik Pehrers <erik.pehrers@jetshop.se>
2019-08-17 23:30:19 +0200

    removed garbage

--- src/Components/Products/Steps/AllSteps.js
+++
@@ -1,5 +0,0 @@
-import Hotdogs from './Hotdogs';
-import Breads from './Breads';
-import Toppings from './Toppings';
-
-export { Hotdogs, Breads, Toppings };
--- src/utils/getProducts.js
+++
@@ -1,9 +0,0 @@
-import { client } from '../index';
-import { GET_ALL_PRODUCTS } from '../gql/gql';
-
-async function getProducts() {
-  const data = client.query({ query: GET_ALL_PRODUCTS });
-  return data;
-}
-
-export default getProducts;
